c2e7624b08363d4e0e6a8b1a14d24b2d2ca13ad1
guocong <875219399@qq.com>
2024-03-01 10:51:42 +0800

    init

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<el-button type="primary" @click="test">测试按钮</el-button>
+</template>
+
+<script setup>
+import $http from "./axios.js"
+import { ElNotification } from 'element-plus'
+
+function test() {
+  $http.get("/test").then(async (res) => {
+    ElNotification({
+      title: 'Success',
+      message: 'This is a success message',
+      type: 'success',
+    })
+  }).catch(err => {
+    console.log(err)
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/test', (req, res) => {
+  setTimeout(() => {
+    res.json({
+      code: 10000,
+      msg: "操作成功！",
+      data: null
+    })
+  }, 500)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
